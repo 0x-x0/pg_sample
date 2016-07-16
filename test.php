762db0d0ea766ba74af30113935bbafa334e37ed
chetantarale <chetan@shippable.com>
2016-07-16 18:57:07 +0530

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 require_once("sql.php");
 class PGTest extends PHPUnit_Framework_TestCase {
   public function test() {
-    $sql = new SQL();
+    $sql = new PG();
     $doc = $sql->main();
     echo $doc;
     $this->assertNotEquals($doc, true);
